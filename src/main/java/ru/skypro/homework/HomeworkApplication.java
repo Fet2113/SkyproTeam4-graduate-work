04fa66107cff31f640d4b11d7d63ab85f8dfa40f
Fet2113 <super.gjoll@yandex.ru>
2024-02-14 14:14:37 +0300

    Init commit

--- src/main/java/ru/skypro/homework/HomeworkApplication.java
+++ src/main/java/ru/skypro/homework/HomeworkApplication.java
@@ -7,5 +7,4 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HomeworkApplication {
   public static void main(String[] args) {
     SpringApplication.run(HomeworkApplication.class, args);
-  }
-}
+  } }
